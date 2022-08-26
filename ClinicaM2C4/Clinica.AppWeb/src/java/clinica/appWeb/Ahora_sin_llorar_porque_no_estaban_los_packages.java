dbcd8dfbe645b685ce17eb5cefe402c55694f030
Javier Rivera <rf21001ts@esfe.agape.edu.sv>
2022-08-26 10:25:04 -0600

    .

---
+++ ClinicaM2C4/Clinica.AppWeb/src/java/clinica/appWeb/Ahora_sin_llorar_porque_no_estaban_los_packages.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clinica.appWeb;
+
+/**
+ *
+ * @author Javier Rivera
+ */
+public class Ahora_sin_llorar_porque_no_estaban_los_packages {
+    
+}
